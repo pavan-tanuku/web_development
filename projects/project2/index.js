c87f98b408ed2eebf47e769da77488d082018057
pavan <pavan.tanuku@sasi.ac.in>
2025-12-23 22:40:15 +0530

    update the project2 - added operators

--- projects/project2/index.js
+++ projects/project2/index.js
@@ -7,10 +7,16 @@ function displayNumber(btn) {
         inputEl.value += btn.innerText;
     } else {
         console.log("It's NaN");
-        
+        const lastDigit = inputEl.value[inputEl.value.length - 1];
+        // converting the last digit into number
+        const lastDigitCasting = Number(lastDigit);
+        if(lastDigit !== "" && (lastDigitCasting >= 0 && lastDigitCasting <= 9)) {
+            inputEl.value += btn.innerText;
+        }
     }
 }
 
+// clear the display
 function clearDisplay() {
     inputEl.value = "";
 }
